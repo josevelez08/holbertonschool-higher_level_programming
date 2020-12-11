1b91ff2e73da8da572d135ad025cbdbf69e68b77
josevelez08 <jose-0919@hotmail.com>
2020-12-11 00:53:38 +0000

    Tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 import sys
-
 if __name__ == "__main__":
     count = 0
     if len(sys.argv) == 1:
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
