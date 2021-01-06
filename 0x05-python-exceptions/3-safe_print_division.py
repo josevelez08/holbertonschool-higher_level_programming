64e6dd2d6043914d18eb9c746452fc455b7c7d8b
josevelez08 <jose-0919@hotmail.com>
2021-01-06 18:36:16 +0000

    Task 3:  integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+
+    try:
+        c = a / b
+        print("Inside result: {}".format(c))
+
+    except:
+        c = None
+        print("Inside result: {}".format(c))
+
+    finally:
+        return (c)
