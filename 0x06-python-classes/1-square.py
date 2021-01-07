a2028daad8bf8ab00f6668c4418c82305e22c06b
josevelez08 <jose-0919@hotmail.com>
2021-01-07 03:23:33 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """ It is the class for define a square: My first class"""
+    __size = ''
+
+    def __init__(self, size):
+        self.__size = size
