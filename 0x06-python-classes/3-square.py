274198af1ba0403f819dab8c24441ec484fb33b8
josevelez08 <jose-0919@hotmail.com>
2021-01-20 10:17:16 +0000

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """ It is the class for define a square: My first class"""
+    __size = ''
+
+    def __init__(self, size=0):
+        self.__size = size
+        try:
+            "{:d}".format(size)
+            if size < 0:
+                raise TypeError
+        except(TypeError):
+            print("size must be >= 0", end="")
+            raise ValueError
+        except(ValueError):
+            print("size must be an integer", end="")
+            raise TypeError
+
+    def area(self):
+        if self.__size:
+            c = self.__size * self.__size
+        return c
