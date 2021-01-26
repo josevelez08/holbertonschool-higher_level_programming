6f1124d8db3ae9a6ca185e8bc381775e391d7eb6
josevelez08 <jose-0919@hotmail.com>
2021-01-26 07:52:53 +0000

    Task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,41 +14,96 @@ class Base:
 
 
 class Rectangle (Base):
-    """The rectangle class """
+
     __width = ''
     __height = ''
     __x = ''
     __y = ''
-    # getter methods
 
-    def get_x(self):
+    @property
+    def x(self):
         return self.__x
 
-    def get_y(self):
+    @x.setter
+    def x(self, value):
+        self.validateInteger(value, "x")
+        self.__x = value
+
+    @property
+    def y(self):
         return self.__y
 
-    def get__height(self):
-        return self.__height
-
-    def get_width(self):
-        return self.__width
-
-    # setter methods
-    def set_x(self, x):
-        self.__x = x
-
-    def set_y(self, y):
+    @y.setter
+    def y(self, y):
+        self.validateInteger(y, "y")
         self.__y = y
 
-    def set_width(self, width):
-        self.__width = width
+    @property
+    def height(self):
+        return self.__height
 
-    def set_height(self, height):
+    @height.setter
+    def height(self, height):
+        self.validateInteger(height, "height")
+        self.validate0(height, "height")
         self.__height = height
 
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.validateInteger(width, "width")
+        self.validate0(width, "width")
+        self.__width = width
+
+    def validateInteger(self, value, name):
+        try:
+            "{:d}".format(value)
+        except:
+            raise TypeError(name + " must be a integer")
+
+    def validate0(self, value, name):
+        if value <= 0:
+            raise ValueError(name + " must be > 0")
+
+    def validate0xy(self, value, name):
+        if value < 0:
+            raise ValueError(name + " must be => 0")
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        self.validateInteger(width, "width")
+        self.validateInteger(height, "height")
+        self.validateInteger(x, "x")
+        self.validateInteger(y, "y")
+        self.validate0(width, "width")
+        self.validate0(height, "height")
+        self.validate0xy(x, "x")
+        self.validate0xy(y, "y")
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    def area(self):
+        height = self.__height
+        width = self.__width
+
+        return width * height
+
+    def display(self):
+        height = self.__height
+        width = self.__width
+        x = self.__x
+        y = self.__y
+        for coordinatesY in range(y):
+            print("")
+        for h in range(height):
+            for w in range(width):
+                if w == 0:
+                    for coordinatesX in range(x):
+                        print(" ", end="")
+                print('#', end=" ")
+            print("")
