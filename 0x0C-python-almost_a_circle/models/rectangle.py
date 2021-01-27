40aed944c1436b6472ea98f81245fd946db6245b
josevelez08 <jose-0919@hotmail.com>
2021-01-27 02:42:30 +0000

    Update code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from models.base import Base
 """ Rentangle file """
+from models.base import Base
 
 
 class Rectangle (Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from models.rectangle import Rectangle
 """Square flie"""
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
