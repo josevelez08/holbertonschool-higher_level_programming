4297a1d1cfa7a83b7c925a87ca3743c14306260e
josevelez08 <jose-0919@hotmail.com>
2021-03-18 01:18:02 +0000

    fix a black space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for number in range(1, 101):
         if (number % 3 == 0) and (number % 5 == 0):
-            print("{}".format("FizzBuzz"), end="")
+            print("{}".format("FizzBuzz"), end=" ")
         elif (number % 3 == 0):
             print("{}".format("Fizz"), end= " ")
         elif (number % 5 == 0):
