018cb4a6bdd4e6220fcbcb2a21833034bbc2ea63
josevelez08 <jose-0919@hotmail.com>
2021-03-23 00:50:06 +0000

    delete the returns

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 if (process.argv[3] != null){
     console.log('Arguments found')
-    return
 }
-if (process.argv[2] != null){
+else if (process.argv[2] != null){
     console.log('Argument found')
-    return
 }
-if (process.argv[2] == null){
+else if (process.argv[2] == null){
     console.log('No argument')
-    return
 }
