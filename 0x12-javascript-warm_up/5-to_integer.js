1035c88544d30cf303a55504b1d9a2e42fbdf0c1
josevelez08 <jose-0919@hotmail.com>
2021-03-23 02:47:04 +0000

    convert to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if(process.argv[2] == null){
+    console.log('Not a number')
+}
+else{
+    if (!isNaN(process.argv[2])) {
+        parseInt(process.argv[2])
+        console.log('My number:', process.argv[2])
+    }
+    else{
+        console.log('Not a number')
+    }
+}
