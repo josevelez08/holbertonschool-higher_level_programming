8ab8cbfdf97612e36efd150fe665e858985315bc
josevelez08 <jose-0919@hotmail.com>
2021-03-23 03:33:45 +0000

    fixing the semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-if (process.argv[3] != null){
-    console.log('Arguments found')
-}
-else if (process.argv[2] != null){
-    console.log('Argument found')
-}
-else if (process.argv[2] == null){
-    console.log('No argument')
+if (process.argv[3] != null) {
+  console.log('Arguments found');
+} else if (process.argv[2] != null) {
+  console.log('Argument found');
+} else if (process.argv[2] == null) {
+  console.log('No argument');
 }
