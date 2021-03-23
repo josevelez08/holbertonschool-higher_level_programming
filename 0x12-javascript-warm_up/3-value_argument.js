0eb1ae7ff0b006891d692e2235d1357b3c19b5ae
josevelez08 <jose-0919@hotmail.com>
2021-03-23 03:38:25 +0000

    fixing the semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] != null){
-    console.log(process.argv[2])
-}
-else if (process.argv[2] == null){
-    console.log('No argument')
+if (process.argv[2] != null) {
+  console.log(process.argv[2]);
+} else if (process.argv[2] == null) {
+  console.log('No argument');
 }
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-if(process.argv[2] == null){
-    console.log('Not a number')
-}
-else{
-    if (!isNaN(process.argv[2])) {
-        parseInt(process.argv[2])
-        console.log('My number:', process.argv[2])
-    }
-    else{
-        console.log('Not a number')
-    }
+if (process.argv[2] == null) {
+  console.log('Not a number');
+} else {
+  if (!isNaN(process.argv[2])) {
+    parseInt(process.argv[2]);
+    console.log('My number:', process.argv[2]);
+  } else {
+    console.log('Not a number');
+  }
 }
