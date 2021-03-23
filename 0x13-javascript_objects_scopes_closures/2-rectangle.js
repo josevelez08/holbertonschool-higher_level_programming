c6f1c3d4559110fa1cf6204866e27e38836ee9eb
josevelez08 <jose-0919@hotmail.com>
2021-03-23 22:30:43 +0000

    Write a class Rectangle that defines a rectangle:

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+constructor (w, h) {
+    if(w == 0 || w < 0 || h == 0 || h < 0 || w == undefined || h == undefined ){
+
+    }
+    else{
+        this.width = w;
+        this.height = h;
+    }
+    }
+}
+module.exports = Rectangle;
