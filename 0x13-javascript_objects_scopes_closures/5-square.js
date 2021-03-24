f963c82906eeec084cf0b65e2b78ba2ed0f9a100
josevelez08 <jose-0919@hotmail.com>
2021-03-24 03:49:50 +0000

    task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle'); //call the parent
+const Rectangle = require('./4-rectangle'); // call the parent
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size); //call the super class constructor
+    super(size, size); // call the super class constructor
   }
 }
 module.exports = Square;
