0181f96deeed30839ac1cbdbe4540abe2c030798
josevelez08 <jose-0919@hotmail.com>
2021-03-24 18:10:51 +0000

    fix the variable y

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 if (!isNaN(process.argv[2])) {
   const occu = parseInt(process.argv[2]);
   let i;
-  const y = 'x';
+  const y = 'X';
   const result = y.repeat(occu);
   for (i = 0; i < occu; i++) {
     console.log(result);
