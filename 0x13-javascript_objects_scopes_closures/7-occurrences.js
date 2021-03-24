2ef2a9b656fafa660a87c65c1b4be1768241ed95
josevelez08 <jose-0919@hotmail.com>
2021-03-24 04:39:02 +0000

    number of ocurrences function

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+exports.nbOccurences = function (list, searchElement) {
+  let i;
+  let count = 0;
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count = count + 1;
+    }
+  }
+  return count;
+};
