6c4091d40f25a342fb0c09973c16c7a9e4c22a52
josevelez08 <jose-0919@hotmail.com>
2021-03-24 03:07:56 +0000

    change the import

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,8 +3,21 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
+    super(size);
     this.size = size;
+  }
+  charPrint(c){
+    if(c){
+      const y = c;
+    }
+    else{
+      const y = 'X';
+    }
+    let i;
+    const result = y.repeat(this.width);
+    for (i = 0; i < this.height; i++) {
+      console.log(result);
+    }
   }
 }
 module.exports = Square;
