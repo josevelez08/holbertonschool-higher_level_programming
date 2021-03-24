ae6beb56440c789c03c1928b15478b98526158d7
josevelez08 <jose-0919@hotmail.com>
2021-03-24 03:44:42 +0000

    delete the charPrint method

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -6,13 +6,5 @@ class Square extends Rectangle {
     super(size);
     this.size = size;
   }
-
-  charPrint (c) {
-    let i;
-    const result = c.repeat(this.size);
-    for (i = 0; i < this.size; i++) {
-      console.log(result);
-    }
-  }
 }
 module.exports = Square;
