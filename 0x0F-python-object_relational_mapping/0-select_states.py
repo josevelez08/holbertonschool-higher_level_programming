9534c2b42156e0ba185a513dd60f3e7fcc27951f
josevelez08 <jose-0919@hotmail.com>
2021-03-27 19:39:22 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import MySQLdb
-import sys
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                                 passwd=sys.argv[2], db=sys.argv[3],
                                 charset="utf8")
