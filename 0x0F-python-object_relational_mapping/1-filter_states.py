a02509f28f7cf4aeca48cfbd29d0af4147e53dd1
josevelez08 <jose-0919@hotmail.com>
2021-03-29 01:46:18 +0000

    tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,14 +3,15 @@
 Module select where the names start with 'n'
 """
 
-import MySQLdb
-import sys
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                                 passwd=sys.argv[2], db=sys.argv[3],
                                 charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE states.name\
+    LIKE BINARY 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
                                 passwd=sys.argv[2], db=sys.argv[3],
                                 charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name = '{}'\
+    ORDER BY id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
