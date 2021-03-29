1d9106ca35e055b7ef803fc47678b0e6121715b5
josevelez08 <jose-0919@hotmail.com>
2021-03-29 01:40:10 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,16 +2,15 @@
 """
 Module select the name that match with a argument
 """
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                                 passwd=sys.argv[2], db=sys.argv[3],
                                 charset="utf8")
-    searchstring = sys.argv[4]
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name='%s'" % (searchstring,))
+    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
