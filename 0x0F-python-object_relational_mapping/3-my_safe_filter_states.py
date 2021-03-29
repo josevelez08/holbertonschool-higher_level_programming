a01c1048a12e3396f0539b853052db97ac038472
josevelez08 <jose-0919@hotmail.com>
2021-03-29 03:15:55 +0000

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module select the name that match with a argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                                passwd=sys.argv[2], db=sys.argv[3],
+                                charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE\
+    states.name =%s ORDER BY id ASC ", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
