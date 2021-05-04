30fee16552250be01daf38d60e77fa29e46c669d
josevelez08 <jose-0919@hotmail.com>
2021-05-04 13:44:34 -0500

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const path = (process.argv[2]);
+const string_to_write = (process.argv[3]);
+fs.writeFile(path, string_to_write, function (err) {
+  if (err) return console.log(err);
+});
