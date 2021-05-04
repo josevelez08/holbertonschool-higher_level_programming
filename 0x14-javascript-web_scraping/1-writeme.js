e0f374c82cc2bd0c85cb8f41584d2c8defbd863f
josevelez08 <jose-0919@hotmail.com>
2021-05-04 16:12:13 -0500

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = (process.argv[2]);
-const string_to_write = (process.argv[3]);
-fs.writeFile(path, string_to_write, 'utf8', function (err) {
+const stringWrite = (process.argv[3]);
+fs.writeFile(path, stringWrite, 'utf8', function (err) {
   if (err) return console.log(err);
 });
