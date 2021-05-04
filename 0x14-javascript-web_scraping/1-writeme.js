b7e1402ce7c8a3e00845cff2e707cd068c2feced
josevelez08 <jose-0919@hotmail.com>
2021-05-04 16:10:52 -0500

    utf8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,6 @@
 const fs = require('fs');
 const path = (process.argv[2]);
 const string_to_write = (process.argv[3]);
-fs.writeFile(path, string_to_write, 'utf8',function (err) {
+fs.writeFile(path, string_to_write, 'utf8', function (err) {
   if (err) return console.log(err);
 });
