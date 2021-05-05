d180a9312e812c7f4a610b9f254494d1fcce79a7
josevelez08 <jose-0919@hotmail.com>
2021-05-04 20:09:20 -0500

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,28 @@
-#!/usr/bin/node​
+#!/usr/bin/node
 
 const url = process.argv[2];
 const request = require('request');
 
-​
 request(url, function (error, response, body) {
-    if (error) {
+  if (error) {
     console.log(error);
-    }
-    const myDict = JSON.parse(body);
-​
-    const userDict = {};
-    let completed = 0;
-    let prevUser = 1;
-​
-    for (const i of myDict) {
-        if (prevUser !== i.userId) {
-        prevUser = i.userId;
-        completed = 0;
-        }
-        if (i.completed) {
-            completed++;
-            userDict[i.userId] = completed;
-        }
-    }
+  }
+  const myDict = JSON.parse(body);
 
-    console.log(userDict);
+  const userDict = {};
+  let completed = 0;
+  let prevUser = 1;
+
+  for (const i of myDict) {
+    if (prevUser !== i.userId) {
+      prevUser = i.userId;
+      completed = 0;
+    }
+    if (i.completed) {
+      completed++;
+      userDict[i.userId] = completed;
+    }
+  }
+
+  console.log(userDict);
 });
